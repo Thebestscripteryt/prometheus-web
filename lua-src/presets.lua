e65e5a7786ed48af675580c8a5063f078f2ffdf9
Thebestscripteryt <shantipriyapal123@gmail.com>
2026-07-04 00:09:42 +0530

    Add files via upload

---
+++ lua-src/presets.lua
@@ -0,0 +1,174 @@
+-- This Script is Part of the Prometheus Obfuscator by Levno_710
+--
+-- pipeline.lua
+--
+-- This Script Provides some configuration presets
+
+return {
+    ["Minify"] = {
+        -- Default is LuaU for Roblox/Luau compatibility (string interpolation, +=, continue, etc.)
+        LuaVersion = "LuaU";
+        -- For minifying no VarNamePrefix is applied
+        VarNamePrefix = "";
+        -- Name Generator for Variables
+        NameGenerator = "MangledShuffled";
+        -- No pretty printing
+        PrettyPrint = false;
+        -- Seed is generated based on current time
+        Seed = 0;
+        -- No obfuscation steps
+        Steps = {
+
+        }
+    };
+    ["Weak"] = {
+        -- Default is LuaU for Roblox/Luau compatibility (string interpolation, +=, continue, etc.)
+        LuaVersion = "LuaU";
+        -- For minifying no VarNamePrefix is applied
+        VarNamePrefix = "";
+        -- Name Generator for Variables that look like this: IlI1lI1l
+        NameGenerator = "MangledShuffled";
+        -- No pretty printing
+        PrettyPrint = false;
+        -- Seed is generated based on current time
+        Seed = 0;
+        -- Obfuscation steps
+        Steps = {
+            {
+                Name = "Vmify";
+                Settings = {
+                    
+                };
+            },
+            {
+                Name = "ConstantArray";
+                Settings = {
+                    Treshold    = 1;
+                    StringsOnly = true;
+                }
+            },
+            {
+                Name = "WrapInFunction";
+                Settings = {
+
+                }
+            },
+        }
+    };
+    ["Medium"] = {
+        -- Default is LuaU for Roblox/Luau compatibility (string interpolation, +=, continue, etc.)
+        LuaVersion = "LuaU";
+        -- For minifying no VarNamePrefix is applied
+        VarNamePrefix = "";
+        -- Name Generator for Variables
+        NameGenerator = "MangledShuffled";
+        -- No pretty printing
+        PrettyPrint = false;
+        -- Seed is generated based on current time
+        Seed = 0;
+        -- Obfuscation steps
+        Steps = {
+            {
+                Name = "EncryptStrings";
+                Settings = {
+
+                };
+            },
+            {
+                Name = "AntiTamper";
+                Settings = {
+                    UseDebug = false;
+                };
+            },
+            {
+                Name = "Vmify";
+                Settings = {
+                    
+                };
+            },
+            {
+                Name = "ConstantArray";
+                Settings = {
+                    Treshold    = 1;
+                    StringsOnly = true;
+                    Shuffle     = true;
+                    Rotate      = true;
+                    LocalWrapperTreshold = 0;
+                }
+            },
+            {
+                Name = "NumbersToExpressions";
+                Settings = {
+
+                }
+            },
+            {
+                Name = "WrapInFunction";
+                Settings = {
+
+                }
+            },
+        }
+    };
+    ["Strong"] = {
+        -- Default is LuaU for Roblox/Luau compatibility (string interpolation, +=, continue, etc.)
+        LuaVersion = "LuaU";
+        -- For minifying no VarNamePrefix is applied
+        VarNamePrefix = "";
+        -- Name Generator for Variables that look like this: IlI1lI1l
+        NameGenerator = "MangledShuffled";
+        -- No pretty printing
+        PrettyPrint = false;
+        -- Seed is generated based on current time
+        Seed = 0;
+        -- Obfuscation steps
+        Steps = {
+            {
+                Name = "Vmify";
+                Settings = {
+                    
+                };
+            },
+            {
+                Name = "EncryptStrings";
+                Settings = {
+
+                };
+            },
+            {
+                Name = "AntiTamper";
+                Settings = {
+
+                };
+            },
+            {
+                Name = "Vmify";
+                Settings = {
+                    
+                };
+            },
+            {
+                Name = "ConstantArray";
+                Settings = {
+                    Treshold    = 1;
+                    StringsOnly = true;
+                    Shuffle     = true;
+                    Rotate      = true;
+                    LocalWrapperTreshold = 0;
+                }
+            },
+            {
+                Name = "NumbersToExpressions";
+                Settings = {
+
+                }
+            },
+            {
+                Name = "WrapInFunction";
+                Settings = {
+
+                }
+            },
+        }
+    },
+}
