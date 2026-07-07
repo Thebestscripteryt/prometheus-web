49bd136dd2ef70a5382b6d27a769672bce74b376
Thebestscripteryt <shantipriyapal123@gmail.com>
2026-07-07 14:47:35 +0530

    Add OpaquePredicates module to steps.lua

--- lua-src/prometheus/steps.lua
+++ lua-src/prometheus/steps.lua
@@ -9,4 +9,5 @@ return {
 	NumbersToExpressions = require("prometheus.steps.NumbersToExpressions");
 	AddVararg 					 = require("prometheus.steps.AddVararg");
 	WatermarkCheck		   = require("prometheus.steps.WatermarkCheck");
+	OpaquePredicates     = require("prometheus.steps.OpaquePredicates");
 }
